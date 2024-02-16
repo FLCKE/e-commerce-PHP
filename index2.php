8fa7ee9bc3e9a7a974c3b11c9300883ceff11254
MOUNIROUWAHAB23 <112950247+MOUNIROUWAHAB23@users.noreply.github.com>
2024-02-16 06:35:47 +0100

    product crud

--- index2.php
+++ index2.php
@@ -5,6 +5,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Bootstrap demo</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">  
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+      <!-- <link rel="stylesheet" href="styles.css" type="text/css"> -->
+    
+    
     <title>Product Dashboard</title>
 </head>
 <body>
@@ -81,15 +88,32 @@ Formulaire de mise à jour
         // Display the product list
         $products = getFromApi($apiUrl);
         // $products = readProducts();
+        
         if (!empty($products) && is_array($products)) {
             foreach ($products as $product) {
-                echo '<li>' . $product['product_name'] . ' - ' . $product['price'] . '</li>';
+                echo '<div class="container">';
+                echo '<div class="row">';
+                // echo '<div class="col-md-4">';
+                echo '<div class="card">';
+                echo '<div class="card-body">';
+                echo '<h5 class="card-title">' . $product['product_name'] . '</h5>';
+                echo '<p class="card-text">' . $product['price'] . '</p>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
             }
         } else {
             echo 'No products found.';
         }
+        
+        
         // 
  ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
 
 
 </div>
