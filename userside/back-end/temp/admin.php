6658b664af6fe739026a41f39894872d9c607ddc
FLCKE <louisfrancisco229@gmail.com>
2024-02-26 18:43:17 +0100

    ajout final

--- userside/back-end/temp/admin.php
+++ userside/back-end/temp/admin.php
@@ -75,7 +75,7 @@ if ($result) {
 <body>
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#"><span>JerseyClub</span></a>
+        <a class="navbar-brand" href="#"><span>E-maillot</span></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
